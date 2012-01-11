c2e1a28eeb3c37c3a281b2b660066d81a9491fe5
GelidElf@Gmail.com <GelidElf@Gmail.com@5884f7cc-b6ee-0f18-bd88-15764fc0c7e1>
2012-01-11 23:08:47 +0000

    intento de eliminar el out of memory elimiando el stringbuffer

---
+++ trunk/planetdominos/src/app/gpwars/util/StringBufferProxy.java
@@ -0,0 +1,23 @@
+package app.gpwars.util;
+
+public class StringBufferProxy {
+
+	public StringBufferProxy(){}
+	
+	public StringBufferProxy(StringBufferProxy proxy){}
+
+	public void append(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int length() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public void append(int owner) {
+		// TODO Auto-generated method stub
+		
+	}
+}
