ad600f40185943214d55ef7463ebc9cd00e5276f
GelidElf@Gmail.com <GelidElf@Gmail.com@5884f7cc-b6ee-0f18-bd88-15764fc0c7e1>
2012-01-30 22:24:07 +0000

    Eliminamos bot viejo Lanzamos el view después de generar el output

--- planetdominos/src/test/createGameOutput.java
+++ planetdominos/src/test/createGameOutput.java
@@ -6,7 +6,8 @@ import java.util.List;
 import app.gpwars.engine.Engine;
 import app.gpwars.engine.Game;
 import app.gpwars.engine.Player;
-import app.planetdominos.engine.bots.Bot300;
+import app.gpwars.viewer.ViewGame;
+import app.planetdominos.engine.bots.BotDandoloTodo;
 
 public class createGameOutput {
 
@@ -14,15 +15,16 @@ public class createGameOutput {
 
 		Engine engine = new Engine();
 
-		Game game = new Game("src/app/gpwars/maps/ourmap1.txt", 200, 0, true);
+		Game game = new Game("src/app/gpwars/maps/ourmap1.txt", 250, 0, true);
 		game.Init();
 
-		Player currentPlayer = new Bot300();
+		Player currentPlayer = new BotDandoloTodo();
 		List<String> LISTA_NOMBRE_BOTS = new ArrayList<String>();
-		LISTA_NOMBRE_BOTS.add("RandomBot");
-		LISTA_NOMBRE_BOTS.add("RandomBot");
-		LISTA_NOMBRE_BOTS.add("RandomBot");
-		engine.runGame(game, currentPlayer, LISTA_NOMBRE_BOTS, true);
+		LISTA_NOMBRE_BOTS.add("ProspectorBot");
+		LISTA_NOMBRE_BOTS.add("BullyBot");
+		LISTA_NOMBRE_BOTS.add("RageBot");
+		System.out.println("Gana: "+ engine.runGame(game, currentPlayer, LISTA_NOMBRE_BOTS, true));
+		ViewGame.main(args);
 	}
 
 }
